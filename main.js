d50ac636a6cf456cf1962fb951c846fcc8ff2021
Aidan Lovelace <aidan@aidanlovelace.com>
2019-06-19 13:06:53 -0500

    Added the code for the tutorial

---
+++ main.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const google = require('googleapis').google;
+const jwt = require('jsonwebtoken');
+
+// Google's OAuth2 client
+const OAuth2 = google.auth.OAuth2;
+
+// Including our config file
+const CONFIG = require('./config');
+
+// Creating our express application
+const app = express();
+
+// Allowing ourselves to use cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// Setting up Views
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+app.get('/', function (req, res) {
+  // Create an OAuth2 client object from the credentials in our config file
+  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
+
+  // Obtain the google login link to which we'll send our users to give us access to some data about them
+  const loginLink = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: CONFIG.oauth2Credentials.scopes, // Using the access scopes from our config file
+    prompt: 'consent' // Makes sure we get a refresh token
+  });
+  return res.render("index", { loginLink: loginLink });
+});
+
+app.get('/auth_callback', function (req, res) {
+  // Create an OAuth2 client object from the credentials in our config file
+  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
+
+  if (req.query.error) {
+    // The user did not give us permission.
+    return res.redirect('/');
+  } else {
+    oauth2Client.getToken(req.query.code, function(err, token) {
+      if (err)
+        return res.redirect('/');
+      
+      // Store the credentials given by google into a jsonwebtoken in a cookie called 'jwt'
+      res.cookie('jwt', jwt.sign(token, CONFIG.JWTsecret));
+      return res.redirect('/get_some_data');
+    });
+  }
+});
+
+app.get('/get_some_data', function (req, res) {
+  // Create an OAuth2 client object from the credentials in our config file
+  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
+
+  if (!req.cookies.jwt) {
+    // We haven't logged in
+    return res.redirect('/');
+  }
+
+  // Add this specific user's credentials to our OAuth2 client
+  oauth2Client.credentials = jwt.verify(req.cookies.jwt, CONFIG.JWTsecret);
+
+  // Give us access to the youtube service
+  const service = google.youtube('v3');
+
+  // Get the first five of the user's subscriptions (the channels they're subscribed to)
+  service.subscriptions.list({
+    auth: oauth2Client,
+    mine: true,
+    part: 'snippet,contentDetails',
+    maxResults: 5
+  }).then(response => {
+    // Render the data view, passing the subscriptions to it
+    return res.render('data', { subscriptions: response.data.items });
+  });
+});
+
+// Listen on the port defined in the config file
+app.listen(CONFIG.port, function () {
+  console.log(`Listening on port ${CONFIG.port}`);
+});
